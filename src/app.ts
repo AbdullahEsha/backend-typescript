5e041d1b14283ba81ca4d5d4fd7eca958ee3b061
AbdullahIsha <shahariaresha@gmail.com>
2023-06-13 23:13:05 +0600

    hello world test

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express'
+require('dotenv').config()
+
+const app: Application = express()
+
+const port: any = process.env.PORT
+
+app.use('/', (req: Request, res: Response): void => {
+  res.send('Hello world!')
+})
+
+app.listen(port, (): void => {
+  console.log('SERVER IS UP ON PORT:', port)
+})
